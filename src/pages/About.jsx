c7bc2e187e18931d00105f800cf086e238b31838
Dmytro <DSerSmart@gmail.com>
2022-10-23 17:00:41 +0300

    Router
    NotFound

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+export const About = () => {
+  return (
+    <main>
+      <h1>About us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi,
+        iure. Tempora quasi quidem fuga fugiat explicabo, asperiores aut!
+        Corrupti molestias doloribus officia rerum dicta hic provident accusamus
+        quas totam quia!
+      </p>
+    </main>
+  );
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+  return (
+    <main>
+      <h1>Welcome</h1>
+      <img src="https://via.placeholder.com/960x240" alt="" />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi,
+        iure. Tempora quasi quidem fuga fugiat explicabo, asperiores aut!
+        Corrupti molestias doloribus officia rerum dicta hic provident accusamus
+        quas totam quia!
+      </p>
+    </main>
+  );
+};
