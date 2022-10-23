822a0545e4bef36a017cf990b75a892a74166b5d
Dmytro <DSerSmart@gmail.com>
2022-10-23 20:18:44 +0300

    Маршрутизация занятие 9
    занятие 9, Всё работает

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,26 @@
 import { Route, Routes } from 'react-router-dom';
 import { About } from '../pages/About';
 import { Home } from '../pages/Home';
+import { ProductDetails } from '../pages/ProductDetails';
 import { Products } from '../pages/Products';
-import { NotFound } from '../pages/NotFound';
-import { Container, Header, Logo, Link } from './App.styled';
+import { Mission } from './Mission';
+import { Reviews } from './Reviews';
+import { Team } from './Team';
+import { SharedLayout } from './SharedLayout';
 
 export const App = () => {
   return (
-    <Container>
-      <Header>
-        <Logo>
-          <span role="img" aria-label="jack-o-lantern">
-            🎧
-          </span>{' '}
-          GoMerch Store
-        </Logo>
-        <nav>
-          <Link to="/" end>
-            Home
-          </Link>
-          <Link to="/about">About</Link>
-          <Link to="/products">Products</Link>
-        </nav>
-      </Header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Container>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<ProductDetails />} />
+      </Route>
+    </Routes>
   );
 };
